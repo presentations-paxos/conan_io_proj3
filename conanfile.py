b5ac78f1f1d353ab8ef1de0cd9da3b8ad3d84bcd
Austin Gilbert <ceretullis@gmail.com>
2018-08-02 21:58:12 -0500

    conan: build with Ninja

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ class Proj3Conan(ConanFile):
         self.run("git clone http://gitlab:8080/demo/proj3.git")
 
     def build(self):
-        cmake = CMake(self)
+        cmake = CMake(self,generator="Ninja")
         cmake.configure(source_folder="proj3")
         cmake.build()
 
